5eaafe6ed883ac87159f662a89140ebf4e895324
tender0623 <jhj_email0623@163.com>
2019-05-25 14:53:57 +0800

    我偷偷加了一条注释

--- CGB-DB-SYS-V1.02/src/main/java/com/db/sys/service/SysDeptService.java
+++ CGB-DB-SYS-V1.02/src/main/java/com/db/sys/service/SysDeptService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import com.db.common.vo.Node;
 import com.db.sys.entity.SysDept;
 
+//我在这里偷偷摸摸加了一条注释
 public interface SysDeptService {
 	int saveObject(SysDept entity);
 	int updateObject(SysDept entity);
